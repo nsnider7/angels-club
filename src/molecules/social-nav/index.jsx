5f98e42f5795b45930e7b4dfbdb3179c5c391169
pokerdawgsnft <100887640+pokerdawgsnft@users.noreply.github.com>
2022-08-28 20:05:17 -0500

    Changed social links

--- src/molecules/social-nav/index.jsx
+++ src/molecules/social-nav/index.jsx
@@ -5,22 +5,22 @@ export default function SocialNav() {
     <nav aria-label="Social navigation" className="social-nav">
       <ul className="reset-list">
         <li>
-          <a className="icon icon-twitter" href="https://twitter.com">
+          <a className="icon icon-twitter" href="https://twitter.com/AngelsClub777">
             <span className="visually-hidden">Follow Angels Club Twitter</span>
           </a>
         </li>
         <li>
-          <a className="icon icon-discord" href="https://discord.gg">
+          <a className="icon icon-discord" href="https://discord.gg/84DrHhpbkm">
             <span className="visually-hidden">Join Angels Club on Discord</span>
           </a>
         </li>
         <li>
-          <a className="icon icon-opensea" href="https://opensea.io">
+          <a className="icon icon-opensea" href="https://opensea.io/">
             <span className="visually-hidden">Join Angels Club on OpenSea</span>
           </a>
         </li>
         <li>
-          <a className="icon icon-instagram" href="https://instagram.com">
+          <a className="icon icon-instagram" href="https://www.instagram.com/angels.club666/">
             <span className="visually-hidden">
               Follow Angels Club on Instagram
             </span>
