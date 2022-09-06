e5672ec434fbbbc7477be0c10c5c7f8984658d7d
Schalk Neethling <schalk.neethling@mechanical-ink.co.za>
2022-09-06 23:08:56 +0200

    use swiper

--- src/molecules/the-art/index.jsx
+++ src/molecules/the-art/index.jsx
@@ -1,6 +1,8 @@
-import { useEffect, useState } from "react";
-import Carousel from "nuka-carousel";
+import { Navigation } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
 
+import "swiper/css";
+import "swiper/css/navigation";
 import "./index.css";
 
 import angel001 from "../../assets/art/1.jpg";
@@ -9,87 +11,72 @@ import angel003 from "../../assets/art/5.jpg";
 import angel004 from "../../assets/art/4.jpg";
 
 export default function TheArt() {
-  const [cellSpacing, setCellSpacing] = useState(0);
-  const [numberOfSlides, setNumberOfSlides] = useState(3);
-
-  useEffect(() => {
-    if (window) {
-      const mq = "(max-width: 47.9375em)";
-      const mqTablet = "(min-width: 47.9375em)";
-      const isMobile = window.matchMedia(mq).matches;
-
-      setCellSpacing(140);
-
-      if (isMobile) {
-        setNumberOfSlides(1);
-      } else {
-        setNumberOfSlides(3);
-      }
-
-      window.matchMedia(mqTablet).addEventListener("change", (event) => {
-        if (event.matches) {
-          setCellSpacing(140);
-        } else {
-          setCellSpacing(0);
-        }
-      });
-
-      window.matchMedia(mq).addEventListener("change", (event) => {
-        if (event.matches) {
-          setNumberOfSlides(1);
-        } else {
-          setNumberOfSlides(3);
-        }
-      });
-    }
-  }, []);
-
   return (
     <div id="the-art" className="the-art-container">
       <h2 className="the-art-title">The Art</h2>
-      <Carousel
-        animation="fade"
-        cellAlign="center"
-        cellSpacing={cellSpacing}
-        defaultControlsConfig={{
-          pagingDotsClassName: "the-art-carousel-paging",
+      <Swiper
+        breakpoints={{
+          320: {
+            slidesPerView: 1,
+            spaceBetween: 20,
+          },
+          768: {
+            slidesPerView: 3,
+            spaceBetween: 40,
+          },
+          1024: {
+            slidesPerView: 4,
+            spaceBetween: 50,
+          },
         }}
-        slidesToShow={numberOfSlides}
-        wrapAround={true}
+        centeredSlides={true}
+        loop={true}
+        modules={[Navigation]}
+        navigation={true}
+        spaceBetween={30}
+        slidesPerView={3}
       >
-        <img
-          className="the-art-gallery-item"
-          src={angel001}
-          width="313"
-          height="313"
-          alt=""
-          loading="lazy"
-        />
-        <img
-          className="the-art-gallery-item"
-          src={angel002}
-          width="313"
-          height="313"
-          alt=""
-          loading="lazy"
-        />
-        <img
-          className="the-art-gallery-item"
-          src={angel003}
-          width="313"
-          height="313"
-          alt=""
-          loading="lazy"
-        />
-        <img
-          className="the-art-gallery-item"
-          src={angel004}
-          width="313"
-          height="313"
-          alt=""
-          loading="lazy"
-        />
-      </Carousel>
+        <SwiperSlide>
+          <img
+            className="the-art-gallery-item"
+            src={angel001}
+            width="313"
+            height="313"
+            alt=""
+            loading="lazy"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="the-art-gallery-item"
+            src={angel002}
+            width="313"
+            height="313"
+            alt=""
+            loading="lazy"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="the-art-gallery-item"
+            src={angel003}
+            width="313"
+            height="313"
+            alt=""
+            loading="lazy"
+          />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img
+            className="the-art-gallery-item"
+            src={angel004}
+            width="313"
+            height="313"
+            alt=""
+            loading="lazy"
+          />
+        </SwiperSlide>
+      </Swiper>
     </div>
   );
 }
